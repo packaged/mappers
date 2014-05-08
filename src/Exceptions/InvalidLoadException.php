124599b3657329709b749692471e770c925e5615
Tom Kay <tom.kay@justdevelop.it>
2014-05-08 14:00:19 +0100

    oops... commit the exceptions

---
+++ src/Exceptions/InvalidLoadException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom Kay
+ * Date: 08/05/2014
+ * Time: 13:50
+ */
+
+namespace Packaged\Mappers\Exceptions;
+
+class InvalidLoadException extends MapperException
+{
+}
---
+++ src/Exceptions/MapperException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom Kay
+ * Date: 08/05/2014
+ * Time: 13:48
+ */
+
+namespace Packaged\Mappers\Exceptions;
+
+class MapperException extends \Exception
+{
+}
